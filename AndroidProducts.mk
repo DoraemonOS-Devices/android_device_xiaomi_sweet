e0581fd2151c0cb8d4e00a57e5b930bf2e1ad421
dopaemon <polarisdp@gmail.com>
2022-02-08 02:56:36 +0000

    Dora: Init commit
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_sweet.mk
+    $(LOCAL_DIR)/dora_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_sweet-eng \
-    arrow_sweet-userdebug \
-    arrow_sweet-user
+    dora_sweet-eng \
+    dora_sweet-userdebug \
+    dora_sweet-user
